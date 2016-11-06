061b83b0c868b44fb26c75344f2dc1872f1bec48
Till Busse <tillbusse@Tills-MacBook-Pro.local>
2016-11-06 08:17:02 +0100

    Fix Nr. 2

---
+++ MatMult/MatMult/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MatMult
+//
+//  Created by Till Busse on 05/11/16.
+//  Copyright © 2016 Till Busse. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
--- MatrixMult/matmult.c
+++ MatrixMult/matmult.c
@@ -1,15 +1,32 @@
+//
+//  main.c
+//  MatMult
+//
+//  Created by Till Busse on 05/11/16.
+//  Copyright © 2016 Till Busse. All rights reserved.
+//
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>                     
-#define DIM 10
+#include <pthread.h>
+#define DIM 100
 #define THREAD_COUNT 10
+#define MAX_ROW_PER_THREAD DIM/10+1
 
 double X[DIM][DIM], M[DIM][DIM], Y[DIM][DIM];
-typedef struct{int rows; int rowCount;} work_t;
-void* matmult(void* work) {        
+typedef struct{
+  int rows[MAX_ROW_PER_THREAD];
+  int rowCount;
+} work_t;
+
+void matmult(void* arg)
+{
   int i,row, col;
-  for (row = 0; row < work->rowCount; row++){
-    for (col = 0; col < DIM; col++) {
+  work_t *work = (work_t*)arg;
+  for (row = 0; row < work->rowCount; row++)
+  {
+    for (col = 0; col < DIM; col++)
+    {
       Y[work->rows[row]][col]=0;
       for (i=0; i < DIM; i++)
         Y[work->rows[row]][col] += X[work->rows[row]][i] * M[i][col];
@@ -19,27 +36,46 @@ void* matmult(void* work) {
 
 int main() {
   int i,j;
-  int lines[DIM / THREAD_COUNT + 1];
-  work_t work[DIM][DIM];
+  work_t work[THREAD_COUNT];
   pthread_t tid[THREAD_COUNT];
 
   for (i=0; i<DIM; i++)
-    for (j=0; j < DIM; j++) {
+  {
+    for (j=0; j < DIM; j++)
+    {
       X[i][j] = drand48(); M[i][j] = drand48();
     }
+  }
+
+  for (j=0; j < THREAD_COUNT; j++)
+  {
+    work[j].rowCount = 0;
+  }
+
 
   for (i=0; i<DIM; i++)
-    for (j=0; j < DIM; j++) {
-      work[i][j].row = i; work[i][j].col = j; 
-      pthread_create(&tid[i][j], NULL, matmult,(void*) &work[i][j]);
-    }
-  for (i=0; i<DIM; i++)
-    for (j=0; j < DIM; j++) 
-      pthread_join(tid[i][j], NULL);
+  {
+    int currentThread = (i % THREAD_COUNT);
+    work[currentThread].rows[work[currentThread].rowCount] = i;
+    work[i % THREAD_COUNT].rowCount++;
+  }
 
-  for (i=0; i<DIM; i++) {
-    for (j=0; j < DIM; j++) 
+  for (j=0; j < THREAD_COUNT; j++)
+  {
+    pthread_create(&tid[j], NULL, (void*)matmult,(void*) &work[j]);
+  }
+
+  for (j=0; j < THREAD_COUNT; j++)
+  {
+    pthread_join(tid[j], NULL);
+  }
+
+  for (i=0; i<DIM; i++)
+  {
+    for (j=0; j < DIM; j++)
+    {
       printf("%f ", Y[i][j]);
-    printf("\n"); 
+    }
+    printf("\n");
   }
 }

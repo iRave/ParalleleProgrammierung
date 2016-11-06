94a42315a04564e33358fa216273be7dbdc6774a
Till Busse <tillbusse@Tills-MacBook-Pro.local>
2016-11-06 12:02:26 +0100

    Git cleanup

--- MatrixMult/matmult.c
+++ MatrixMult/matmult.c
@@ -9,9 +9,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#define DIM 100
-#define THREAD_COUNT 10
-#define MAX_ROW_PER_THREAD DIM/10+1
+#define DIM 2000
+#define THREAD_COUNT 20
+#define MAX_ROW_PER_THREAD DIM/THREAD_COUNT+1
 
 double X[DIM][DIM], M[DIM][DIM], Y[DIM][DIM];
 typedef struct{
@@ -38,38 +38,40 @@ int main() {
   int i,j;
   work_t work[THREAD_COUNT];
   pthread_t tid[THREAD_COUNT];
-
+  
   for (i=0; i<DIM; i++)
   {
     for (j=0; j < DIM; j++)
     {
-      X[i][j] = drand48(); M[i][j] = drand48();
+      X[i][j] = drand48();
+      M[i][j] = drand48();
     }
   }
-
+  
   for (j=0; j < THREAD_COUNT; j++)
   {
     work[j].rowCount = 0;
   }
-
-
+  
+  
   for (i=0; i<DIM; i++)
   {
     int currentThread = (i % THREAD_COUNT);
     work[currentThread].rows[work[currentThread].rowCount] = i;
     work[i % THREAD_COUNT].rowCount++;
   }
-
+  
   for (j=0; j < THREAD_COUNT; j++)
   {
-    pthread_create(&tid[j], NULL, (void*)matmult,(void*) &work[j]);
+    pthread_create(&tid[j], NULL, (void *)matmult,(void*) &work[j]);
   }
-
+  
   for (j=0; j < THREAD_COUNT; j++)
   {
     pthread_join(tid[j], NULL);
   }
-
+  
+  /*
   for (i=0; i<DIM; i++)
   {
     for (j=0; j < DIM; j++)
@@ -78,4 +80,5 @@ int main() {
     }
     printf("\n");
   }
+  */
 }

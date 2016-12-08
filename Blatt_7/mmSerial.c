024ad94bbbc5e0d2a50c15051237b33d6c832f27
TheFloe1995 <floespam@gmail.com>
2016-12-08 13:44:17 +0100

    Minor refactorings

--- Blatt_7/mmSerial.c
+++ Blatt_7/mmSerial.c
@@ -14,11 +14,14 @@ int main()
   double leftMat[MAT_SIZE][MAT_SIZE], rigthMat[MAT_SIZE][MAT_SIZE], resultMat[MAT_SIZE][MAT_SIZE];
 
   init(leftMat, rigthMat, resultMat);
+
   printf("leftMat:\n");
   printMatrix(leftMat);
   printf("\nRigthMat:\n");
   printMatrix(rigthMat);
+
   matMult(leftMat, rigthMat, resultMat);
+
   printf("\nresultMat:\n");
   printMatrix(resultMat);
 }
@@ -26,26 +29,26 @@ int main()
 void init(double leftMat[MAT_SIZE][MAT_SIZE], double rigthMat[MAT_SIZE][MAT_SIZE], double resultMat[MAT_SIZE][MAT_SIZE])
 {
   srand(time(NULL));
-  for(int i = 0; i < MAT_SIZE; i++)
+  for(int row = 0; row < MAT_SIZE; row++)
   {
-    for(int j = 0; j < MAT_SIZE; j++)
+    for(int col = 0; col < MAT_SIZE; col++)
     {
-      leftMat[i][j] = ((double)(rand() % ACCURACY))/ACCURACY;
-      rigthMat[i][j] = ((double)(rand() % ACCURACY))/ACCURACY;
-      resultMat[i][j] = 0.0;
+      leftMat[row][col] = ((double)(rand() % ACCURACY))/ACCURACY;
+      rigthMat[row][col] = ((double)(rand() % ACCURACY))/ACCURACY;
+      resultMat[row][col] = 0.0;
     }
   }
 }
 
 void matMult(double leftMat[MAT_SIZE][MAT_SIZE], double rigthMat[MAT_SIZE][MAT_SIZE], double resultMat[MAT_SIZE][MAT_SIZE])
 {
-  for(int i = 0; i < MAT_SIZE; i++)
+  for(int row = 0; row < MAT_SIZE; row++)
   {
-    for(int j = 0; j < MAT_SIZE; j++)
+    for(int col = 0; col < MAT_SIZE; col++)
     {
-      for(int k = 0; k < MAT_SIZE; k++)
+      for(int element = 0; element < MAT_SIZE; element++)
       {
-        resultMat[i][j] += leftMat[i][k] * rigthMat[k][j];
+        resultMat[row][col] += leftMat[row][element] * rigthMat[element][col];
       }
     }
   }
@@ -53,11 +56,11 @@ void matMult(double leftMat[MAT_SIZE][MAT_SIZE], double rigthMat[MAT_SIZE][MAT_S
 
 void printMatrix(double matrix[MAT_SIZE][MAT_SIZE])
 {
-  for (int i = 0; i < MAT_SIZE; i++)
+  for (int row = 0; row < MAT_SIZE; row++)
   {
-    for (int j = 0; j < MAT_SIZE; j++)
+    for (int col = 0; col < MAT_SIZE; col++)
     {
-      printf("| %.2f", matrix[i][j]);
+      printf("| %.2f", matrix[row][col]);
     }
     printf("\n");
   }
